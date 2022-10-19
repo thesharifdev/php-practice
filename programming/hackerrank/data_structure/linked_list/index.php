90b7008ef7520b32769fe667bfd3606d4a61e824
Md Sharif Miah <sharif.golang@gmail.com>
2022-10-19 19:52:13 +0600

    tried 2nd time but still not accepted

--- programming/hackerrank/data_structure/linked_list/index.php
+++ programming/hackerrank/data_structure/linked_list/index.php
@@ -1,31 +1,39 @@
 <?php
+$_fp = fopen("php://stdin", "r");
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */
 
 class SinglyLinkedListNode {
-
+    
     public $data;
     public $next;
-
-    public function __construct($data){
+    
+    public function __construct($data = null){
         $this->data = $data;
-        $this->next = null;
     }
 }
 
-function insertNodeAtTail($head, $data){
+class LinkedList {
+    
+    public $head;
+}
+
+function insertNodeAtTail (LinkedList $linked_list, $data = null){
     
     $new_node = new SinglyLinkedListNode($data);
-   
-    if($head == null){
-        $head = $new_node;
+    $linked_list = new $linked_list;
+    
+    if($linked_list->head == null){
+        $linked_list->head = $new_node;
     }else{
-        $current_node = $head;
-
+        $current_node = $linked_list->head;
         while($current_node->next != null){
-            $temp_node = $current_node->next;
+            $current_node = $current_node->next;
         }
-
-        $temp_node->next = $new_node;
+        
+        $current_node->next = $new_node;
     }
-
-    return $head;
+    
+    return $new_node;
 }
+
+?>
